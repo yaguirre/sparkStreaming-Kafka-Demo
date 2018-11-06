4e72473c22e046df57a442e1ffc3ec8cbcb22333
yaguirre <yaguirre@eafit.edu.co>
2018-11-06 10:22:16 -0500

    Funciona spark streaming, analiza sentimientos y grafica

--- implementacion/twitterStream.py
+++ implementacion/twitterStream.py
@@ -4,7 +4,8 @@ from pyspark.streaming.kafka import KafkaUtils
 import operator
 import numpy as np
 import matplotlib.pyplot as plt
-
+from pylab import savefig
+import datetime
 
 def main():
     conf = SparkConf().setMaster("local[2]").setAppName("Streamer")
@@ -15,7 +16,7 @@ def main():
     ssc.checkpoint("checkpoint")
     pwords = load_wordlist("./Dataset/positive.txt")
     nwords = load_wordlist("./Dataset/negative.txt")
-    counts = stream(ssc, pwords, nwords, 100)
+    counts = stream(ssc, pwords, nwords, 60)
     make_plot(counts)
 
 
@@ -27,6 +28,9 @@ def make_plot(counts):
     negativeCounts = []
     time = []
 
+    print("!!!!!!!!!!!!!PRINTING COUNTS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print(counts)
+    
     for val in counts:
         positiveTuple = val[0]
         positiveCounts.append(positiveTuple[1])
@@ -36,6 +40,7 @@ def make_plot(counts):
     for i in range(len(counts)):
         time.append(i)
 
+    fig = plt.figure()
     posLine = plt.plot(time, positiveCounts,'bo-', label='Positive')
     negLine = plt.plot(time, negativeCounts,'go-', label='Negative')
     plt.axis([0, len(counts), 0, max(max(positiveCounts), max(negativeCounts))+50])
@@ -43,7 +48,9 @@ def make_plot(counts):
     plt.ylabel('Word count')
     plt.legend(loc = 'upper left')
     plt.show()
-
+    fig.savefig("/home/yorman/Desktop/plot" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ".png")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    
 
 def load_wordlist(filename):
     """ 
@@ -54,6 +61,7 @@ def load_wordlist(filename):
     text = f.read()
     text = text.split('\n')
     for line in text:
+        line = line.encode("ascii", "Ignore")
         words[line] = 1
     f.close()
     return words
@@ -78,6 +86,11 @@ def sendRecord(record):
     connection.close()
 
 
+def splitLine(line):
+    result = line.split(b' ')
+    return result
+
+
 def stream(ssc, pwords, nwords, duration):
     kstream = KafkaUtils.createDirectStream(
     ssc, topics = ['twitterstream'], kafkaParams = {"metadata.broker.list": 'localhost:9092'})
@@ -85,7 +98,7 @@ def stream(ssc, pwords, nwords, duration):
 
     # Each element of tweets will be the text of a tweet.
     # We keep track of a running total counts and print it at every time step.
-    words = tweets.flatMap(lambda line:line.split(" "))
+    words = tweets.flatMap(lambda line: splitLine(line))
     positive = words.map(lambda word: ('Positive', 1) if word in pwords else ('Positive', 0))
     negative = words.map(lambda word: ('Negative', 1) if word in nwords else ('Negative', 0))
     allSentiments = positive.union(negative)
@@ -96,12 +109,15 @@ def stream(ssc, pwords, nwords, duration):
     # The counts variable hold the word counts for all time steps
     counts = []
     sentimentCounts.foreachRDD(lambda t, rdd: counts.append(rdd.collect()))
+    sentimentCounts.pprint()
     
     # Start the computation
     ssc.start() 
     ssc.awaitTerminationOrTimeout(duration)
     ssc.stop(stopGraceFully = True)
 
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!STOPPED!!!!!!!!!!!!!!!!!!!!!!!!")
+    
     return counts
 
 
